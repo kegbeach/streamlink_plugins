4193940699b645a1bd24fed29010770b06493407
kegbeach <81620868+kegbeach@users.noreply.github.com>
2022-02-01 23:11:49 -0500

    Add files via upload

---
+++ camsoda.py
@@ -0,0 +1,89 @@
+import random
+import re
+import json
+
+from streamlink.plugin import Plugin
+from streamlink.plugin.api import http
+from streamlink.plugin.api import validate
+from streamlink.plugin.api import useragents
+from streamlink.stream import HLSStream
+
+_url_re = re.compile(r"http(s)?://(www\.)?camsoda\.com/(?P<username>[^\"\']+)")
+
+_api_user_schema = validate.Schema(
+    {
+        "status": validate.any(int, validate.text),
+        validate.optional("user"): validate.Schema({
+            "chat": validate.Schema ({
+                    "status": validate.any(int, validate.text)
+            })
+        })
+    }
+)
+
+_api_video_schema = validate.Schema(
+    {
+        "token": validate.text,
+        "edge_servers": [validate.text],
+        "stream_name": validate.text
+    }
+)
+
+
+class Camsoda(Plugin):
+    API_URL_VIDEO = "https://www.camsoda.com/api/v1/video/vtoken/{0}?username=guest_{1}"
+    HLS_URL_VIDEO_EDGE = "https://{server}/{stream_name}_h264_aac_720p/index.m3u8?token={token}"
+    HLS_URL_VIDEO = "https://{server}/mp4:{stream_name}_aac/playlist.m3u8?token={token}"
+    headers = {
+        "User-Agent": useragents.FIREFOX
+    }
+
+    @classmethod
+    def can_handle_url(cls, url):
+        return _url_re.match(url)
+
+    def _stream_status(self, data_user):
+
+        invalid_username = data_user["status"] is False
+        if invalid_username:
+            self.logger.info("No validate username found for {0}".format(self.url))
+            return
+
+        is_online = data_user["status"] is True and data_user["user"]["chat"]["status"] == "online"
+        if is_online is False:
+            self.logger.info("Stream is currently offline or private")
+            return
+
+        return True
+
+    def _get_api_video(self, username):
+        res = http.get(self.API_URL_VIDEO.format(username, str(random.randint(1000, 99999))), headers=self.headers, verify=False)
+        data_video = http.json(res, schema=_api_video_schema)
+        return data_video
+
+    def _get_streams(self):
+        match = _url_re.match(self.url)
+        username = match.group("username")
+        username = username.replace("/", "")
+
+        data_video = self._get_api_video(username)
+
+        if data_video["edge_servers"]:
+            hls_url = self.HLS_URL_VIDEO.format(
+                server=data_video["edge_servers"][0],
+                stream_name=data_video["stream_name"],
+                token=data_video["token"]
+            )
+            if "edge" in data_video["edge_servers"][0]:
+                self.session.http.verify = False
+                hls_url = self.HLS_URL_VIDEO_EDGE.format(
+                    server=data_video["edge_servers"][0],
+                    stream_name=data_video["stream_name"],
+                    token=data_video["token"]
+                )
+
+            for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
+                yield s
+
+
+__plugin__ = Camsoda
---
+++ chaturbate.py
@@ -0,0 +1,56 @@
+# chaturbate.com plugin for streamlink 2.3.0+
+# updated for new url matching api
+#
+# modified by kegbeach
+#
+
+import re
+import uuid
+
+from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.plugin.api import validate
+from streamlink.stream import HLSStream
+
+API_HLS = "https://chaturbate.com/get_edge_hls_url_ajax/"
+
+_post_schema = validate.Schema(
+    validate.all(
+    {
+        "url": validate.text,
+        "room_status": validate.text,
+        "success": int
+    }
+))
+
+@pluginmatcher(re.compile(
+    r"https?://(\w+\.)?chaturbate\.com/(?P<username>\w+)"
+))
+
+class Chaturbate(Plugin):
+    def _get_streams(self):
+        username = self.match.group("username")
+
+        CSRFToken = str(uuid.uuid4().hex.upper()[0:32])
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-CSRFToken": CSRFToken,
+            "X-Requested-With": "XMLHttpRequest",
+            "Referer": self.url,
+        }
+
+        cookies = {
+            "csrftoken": CSRFToken,
+        }
+
+        post_data = "room_slug={0}&bandwidth=high".format(username)
+
+        res = self.session.http.post(API_HLS, headers=headers, cookies=cookies, data=post_data)
+        data = self.session.http.json(res, schema=_post_schema)
+
+        self.logger.info("Stream status: {0}".format(data["room_status"]))
+        if (data["success"] is True and data["room_status"] == "public" and data["url"]):
+            for s in HLSStream.parse_variant_playlist(self.session, data["url"]).items():
+                yield s
+
+__plugin__ = Chaturbate
